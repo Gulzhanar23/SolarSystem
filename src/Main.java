3d52ee907bd78e46d22ec0072322ce429640546d
Gulzhanar23 <berikgulzhanar@gmail.com>
2023-03-28 20:38:08 -0400

    i have successfully resolved the issue and code is ready to be merged in the sun branch

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,13 @@
 public class Main {
     public static void main(String[] args) {
-        SolarSystem solar=new SolarSystem();
-        solar.sun="1";
-        solar.moon="2";
-        solar.planets="2";
+        SolarSystem solar = new SolarSystem();
+        solar.sun = "1";
+        solar.moon = "2";
+        solar.planets = "2";
 
-        featureSun Xron=new featureSun();
-        Xron.temperature="1234567k";
-        Xron.radius=1234567;
-        Xron.color="pink";
+        featureSun Xron = new featureSun();
+        Xron.temperature = "1234567k";
+        Xron.radius = "1234567";
+        Xron.color = "pink";
     }
 }
